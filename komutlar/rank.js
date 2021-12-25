0908352aa7a0fd847a1bcef8f89bc425d7f1ca6b
Glitch (glitch-hello-node) <none>
2021-12-25 20:31:08 +0000

    🛀🚍 Checkpoint ./komutlar/rank.js:40357916/474 ./komutlar/rank2.js:40357916/146

---
+++ komutlar/rank.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+name:"rank",
+
+code:`
+
+$color[00001]
+
+$image[https://vacefron.nl/api/rankcard?username=$replaceText[$username[$mentioned[1;yes]]%23$discriminator[$mentioned[1;yes]]; ;+;-1]&avatar=$userAvatar[$mentioned[1;yes]]?size=4096&level=$getUserVar[level;$mentioned[1;yes]]&rank=&currentxp=$getUserVar[talk;$mentioned[1;yes]]&nextlevelxp=$getUserVar[req;$mentioned[1;yes]]&previouslevelxp=0&custombg=$getUserVar[Rankcard]&xpcolor=000000&isboosting=false]  
+
+`
+
+} 
+
---
+++ komutlar/rank2.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+name:"$alwaysExecute",
+
+code:`
+
+$setUserVar[talk;$sum[5;$getUserVar[talk]]]
+
+$onlyForServers[$getUserVar[level];]]  
+
+`
+
+} 
+
+p
