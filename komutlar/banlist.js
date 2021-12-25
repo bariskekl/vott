926fa35ec12758cb7d0d23eedebc78c663cb9c5e
Glitch (vortexbottx) <none>
2021-12-25 21:39:17 +0000

    🚌🍠 Checkpoint ./komutlar/banlist.js:40357916/886

--- komutlar/banlist.js
+++ komutlar/banlist.js
@@ -1,12 +1,12 @@
 module.exports = {
   name: "banlist",
   code: `
-  $title[$serverName[$guildID]]
+$author[$serverName[$guildID];$serverIcon]
   $color[RANDOM]
   $description[
   $replaceText[$djsEval[message.guild.fetchBans() .then(x => x.map(member => member.user.tag + " " + "-" + " " +member.user.id).join("NJOİNN"));yes];NJOİNN;
 ;-1]]
-$thumbnail[$serverIcon]
+
 $onlyPerms[ban;Komutu Kullanmak İçin \`Üyeleri Banla\` İznine Sahip Olman Gerek
 `
-}
+};
