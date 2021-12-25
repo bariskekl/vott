27558f21a9eb244ff029beed4f9d688ee067dd62
Glitch (vortexbottx) <none>
2021-12-25 21:28:11 +0000

    🏠👡 Checkpoint ./komutlar/banlist.js:40357916/4089 ./komutlar/bancount.js:40357916/679

---
+++ komutlar/bancount.js
@@ -0,0 +1,19 @@
+module.exports = {
+  name: "bancount",
+
+  code: `
+
+$color[RANDOM]
+
+$author[$serverName[$guildID];$serverIcon]
+
+$description[
+
+🤝 | Sunucuda Toplam Banlı Kişi Sayısı \`$banCount\` Kişidir.
+
+]
+
+$footer[$addTimestamp]
+
+`
+};
