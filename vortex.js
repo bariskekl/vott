9b30858ddde0f5bef8e6dfc463cebb6560954aa8
Glitch (vortexbottx) <none>
2021-12-27 12:26:20 +0000

    🏉☂️ Checkpoint ./vortex.js:40357916/23012

--- vortex.js
+++ vortex.js
@@ -448,31 +448,11 @@ $botTyping[31h]
 });
 
 bot.command({
-name:"test",
-code:`
+  name: "test",
+  code: `
 
-
-$ban[$mentioned[1]]
-
-$color[$random[0;999999]]
-
-$description[
-
-📚 | Banlayan Yetkili : <@$authorID>
-
-📚 | Banlanan Üye : <@$mentioned[1]>
-
-📚 | Banlanma Sebebi : \`$noMentionMessage\`
-
-]
-
-$onlyPerms[ban;📚 | Bunun İçin \`Üyeleri Banla\` İznine Sahip Olman Gerek]
-
-$onlyBotPerms[ban;📚 | Birisini Banlamak İçin Bana \`Üyeleri Banla\` İzni Vermelisin]
-
-$argsCheck[<1;📚 | Birini Etiketlemelisin] 
 
 
 
 `
-  })
+});
