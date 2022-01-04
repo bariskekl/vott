7268f3afd503c13f2fef78ced17cd2f38f0c4345
Glitch (vortexbottx) <none>
2022-01-04 10:42:57 +0000

    🙍👆 Checkpoint ./komutlar/ban.js:40357916/1753

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -1,28 +1,12 @@
 module.exports = {
-
   name: "ban",
-
   code: `
-
-$author[$userTag[$mentioned[1]] Sunucudan Banlandı;$userAvatar[$mentioned[1]]]
-
-$description[Sebebi : $replaceText[**$noMentionMessage;**;;-1]]
-
-$ban[$mentioned[1];$noMentionMessage;1]
-
-$onlyIf[$rolePosition[$highestRole[$authorID]]<=$rolePosition[$highestRole[$mentioned[1]]];⚠️ | Rolünden Üstte Birini Banlamazsın]
-
-$onlyBotPerms[ban;⚠️ | Bunun İçin Botun  \`Üyeleri Yasakla\` İzni Olmalı]
-
-$onlyIf[$mentioned[1]!=;✌️ | Kimi banlamalıyım bunu tüm gün düşünücem ^^]
-
-$onlyPerms[ban;⚠️ | Bunun İçin \`Üyeleri Yasakla\` İznin Olmalı]
-
-$suppressErrors[⚠️ | Rolümden Üstte Birini Banlayamam]
-
-$onlyIf[$getGlobalUserVar[kl;$authorID]!=true;\`$getGlobalUserVar[ksebep;$authorID]\` sebebinden karalistedesiniz.] 
-
+$dm[$mentioned[1]]
+$ban[$noMentionMessage;$mentioned[1]] 
+$color[RANDOM]
+$description[
+<@$mentioned[1]> \`$serverName\` İsimli Sunucudan \`$noMentionMessage\` Sebebi İle \` Banlandın]
+$channelSendMessage[$channelID;
+<@$mentioned[1]> Başarıyla \`$noMentionMessage\` Sebebi İle Banlandı]
 `
-
 };
-
