aff8195c95487e47904cc7dea8923b449d026745
Glitch (vortexbottx) <none>
2022-01-04 11:17:09 +0000

    🍓⛺️ Checkpoint ./komutlar/unban.js:40357916/1746 ./komutlar/ban.js:40357916/144

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -6,8 +6,9 @@ $ban[$mentioned[1];$noMentionMessage]
 $description[<:emoji_70:917309183341236244> | \`$userTag[$mentioned[1]]\` İsimli Kullanıcı \`$noMentionMessage\` Sebebi İle Banlandı ^^]
 $color[RANDOM]
 $footer[$addTimestamp]
-$onlyPerms[ban;<:emoji_71:917309220687314966> | Bu Komutu Kullanamazsın Kullanmak İçin \`Üyeleri Banla\` Yetkin Olmalı]
-$onlyBotPerms[ban;<:emoji_71:917309220687314966> | Bu Komutu Kullanamazsın Kullanmak İçin Bana \`Üyeleri Banla\` Yetkisi Vermelisin ^^]
+$onlyPerms[ban;<:emoji_71:917309220687314966> | Bu Komutu Kullanamazsın Kullanmak İçin \`Üyeleri Banla\` Yetkin Olmalı ^^]
+$onlyBotPerms[ban;<:emoji_71:917309220687314966> | Bu Komutu Kullanmak İçin Bana \`Üyeleri Banla\` Yetkisi Vermelisin ^^]
 $suppressErrors
+$argsCheck[>1;<:emoji_71:917309220687314966> | Banlayacağım Üyeyi Etiketlemelisin]
 `
 };
--- komutlar/unban.js
+++ komutlar/unban.js
@@ -1,17 +1,15 @@
 module.exports = {
-
   name: "unban",
 
   code: `
 $author[$userTag[$message[1];$userAvatar[$message[1]]]]
 $color[RANDOM]
 $unban[$message[1]]
-$description[
-\`$userTag[$message[1]]\` İsimli Kullanıcın Banı Başarıyla Kaldırıldı Banlanma Sebebi İse \`$getBanReason[$message[1]]\`
-]
+$description[<:emoji_70:917309183341236244> | \`$userTag[$message[1]]\` İsimli Kullanıcın Banı Başarıyla Kaldırıldı]
 $footer[$addTimestamp]
-$onlyPerms[ban;<:emoji_71:917309220687314966> | Bu Komutu Kullanamazsın Kullanmak İçin \°
+$onlyPerms[ban;<:emoji_71:917309220687314966> | Bu Komutu Kullanamazsın Kullanmak İçin \`Üyeleri Banla\` Yetkin Olmalı]
+$onlyBotPerms[ban;<:emoji_71:917309220687314966> | Bu Komutu Kullanmam İçin \`Üyeleri Banla\` Yetkim Olmalı]
+$suppressErrors
+$argsCheck[>1;<:emoji_71:917309220687314966> | Bir Kullanıcı ID'si Girmelisin]
 `
-
 };
-
