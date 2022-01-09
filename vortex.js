a8553537ccd3d0b3da163fabff419bd33cde9849
Glitch (vortexbottt31) <none>
2022-01-09 18:00:51 +0000

    🍣🍼 Checkpoint ./vortex.js:40357916/23719 ./package.json:40357916/6

--- vortex.js
+++ vortex.js
@@ -1,24 +1,28 @@
-var dbd = require(`aoi.js`);
+var vortex = require(`aoi.js`);
 var fs = require("fs");
 var bot = new vortex.Bot({
   token: process.env.token,
-  prefix: "?"
+  prefix: "?",
 });
 
+bot.onJoined()
+bot.onLeave()
 bot.onMessage();
-var reader = fs.readdirSync("./komutlar").filter(file => file.endsWith(".js"));
+var reader = fs
+  .readdirSync("./komutlar")
+  .filter((file) => file.endsWith(".js"));
 for (const file of reader) {
   const command = require(`./komutlar/${file}`);
   bot.command({
     name: command.name,
-    code: command.code
+    code: command.code,
   });
 }
 
 bot.status({
   text: "Ben Yeni Bir Botum",
   type: "PLAYING",
-  time: 12
+  time: 12,
 });
 
 bot.command({
