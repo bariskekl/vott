0215a4d11ec3cee7a60c438c940ae2fe4bb80373
Glitch (vortexbottt31) <none>
2022-01-10 16:22:20 +0000

    🐡🕵 Checkpoint ./komutlar/kick.js:40357916/1407

--- komutlar/kick.js
+++ komutlar/kick.js
@@ -1,17 +1,13 @@
 module.exports = {
-
   name: "kick",
-
-  aliases: "kick",
-
   code: `
+$author[$userTag[$mentioned[1]];$userAvatar[$mentioned[1]]
 $kick[$mentioned[1];$noMentionMessage]
 $color[RANDOM]
 $description[
-\`$userTag[$mentioned[1]] \` İsimli Kullanıcı \`$noMentionMessage\` Sebebinden Sunucudan Atıldı]
+<:emoji_70:917309183341236244> | \`$userTag[$mentioned[1]] \` İsimli Kullanıcı \`$noMentionMessage\` Sebebinden Sunucudan Atıldı]
 $onlyPerms[kick;{description:<:emoji_71:917309220687314966> | Bu Komutu Kullanmak İçin \`Üyeleri At\` İznin Olmalı}{color:RANDOM}
-$onlyIf[$me
-`
-
+$argsCheck[>1;{description:<:emoji_71:917309220687314966> | Bir Üye Etiketlemelisin Dostum}{color:RANDOM}]
+$footer[$addTimestamp]
+`,
 };
-
