91d868366a22e28e73d1da5d0519d0b39691360f
Glitch (vortexbottt31) <none>
2022-01-11 14:25:52 +0000

    🌓😹 Checkpoint ./komutlar/forceban.js:40357916/27 ./komutlar/moderasyon.js:40357916/200

--- komutlar/forceban.js
+++ komutlar/forceban.js
@@ -4,7 +4,7 @@ module.exports = {
 $ban[$message[1]]
 $color[RANDOM]
 $description[
-<:emoji_70:917309183341236244> | \`$userTag[$message[1]\` İsimli Kullanıcı \`$userTag[$authorID] Tarafından Banlandı]
+<:emoji_70:917309183341236244> | \`$userTag[$message[1]]\` İsimli Kullanıcı \`$userTag[$authorID]\` Tarafından Banlandı]
 $onlyPerms[admin;{description:<:emoji_71:917309220687314966> | Bu Komutu Kullanmak İçin \`Yönetici\` İznin Olmalı}{color:RANDOM}]
 $argsCheck[>1;{description:<:emoji_71:917309220687314966> | Bir Kullanıcı IDsi Girmelisin}{color:RANDOM}]
 
