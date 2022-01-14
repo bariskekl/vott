d647f8dbe182eac784bdebf358ec3daaaa346368
Glitch (vortexbottt31) <none>
2022-01-14 14:11:23 +0000

    🏏😇 Checkpoint ./vortex.js:40357916/26466

--- vortex.js
+++ vortex.js
@@ -548,16 +548,17 @@ bot.variables({
   admin: "",
 });
 
-
-
- bot.command({
-  name:"force-ban",
-   code:`
+bot.command({
+  name: "force-ban",
+  code: `
   $ban[$message[1]]
   $author[$userTag[$authorID];$userAvatar[$authorID]]
+  $thumbnail[$userAvatar[$message[1]]]
   $description[
-  <:emoji_70:917309183341236244>| \`$userTag[$message[1]]\` İsimli Kullanıcı Başarıyla Banlandı]
-   $onlyPerms[admin;{description:<:emoji_71:917309220687314966> 
+  <:emoji_70:917309183341236244> | \`$userTag[$message[1]]\` İsimli Kullanıcı Başarıyla Banlandı]
+  $footer[Kullanıcı ID'si : **$message[1]**;$userAvatar[$message[1]]]
+  $onlyPerms[ban;{description:<:emoji_71:917309220687314966> | \`$userTag[$authorID]\` Bunu Kullanmak İçin \`Üyeleri Banla İznin\` Olmalı}{color:RANDOM}]
+   $argsCheck[>1;{description:<:emoji_71:917309220687314966> | Bir Kullanıcı ID'si Girmelisin Dostum}{color:RANDOM}]
    
-   `
- })
+   `,
+});
