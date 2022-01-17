080f0cd30bb817cb55bc68393a9b48431233844c
Glitch (vortexbottt31) <none>
2022-01-17 16:31:49 +0000

    🌗🐴 Checkpoint ./vortex.js:40357916/29602

--- vortex.js
+++ vortex.js
@@ -612,11 +612,15 @@ $description[
 });
 
 bot.command({
-name:"devpara",
-code:`
+  name: "devpara",
+  code: `
 $color[RANDOM]
-$authot[$userTag[$authorID];$userAvatar[$authorID]]
+$author[$userTag[$authorID];$userAvatar[$authorID]]
+$description[
+<:emoji_70:917309183341236244> | \`$userTag[$authorID]\` **Başarıyla** \`900000000\` **Miktarında Para Aldın**]
+ $setGlobalUserVar[para;$sum[$getGlobalUserVar[para;$authorID];900000000];$authorID]
 
-
-`
-  })
+  
+$onlyForIDs[921073149259427910;754320168469135440;{description:<:emoji_71:917309220687314966> | \`$userTag[$authorID]\` Bunu Kullanmak İçin Developer Olmalısın}{color:RANDOM}]
+`,
+});
